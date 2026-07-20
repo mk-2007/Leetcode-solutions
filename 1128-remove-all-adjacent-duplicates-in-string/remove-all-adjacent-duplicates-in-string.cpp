8f138736273237f30830601077f88f71027525a3
Mahandar Khatri <mk6447959@gmail.com>
2026-07-20 16:08:40 +0500

    Time: 4 ms (69.29%) | Memory: 13.6 MB (73.02%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        stack<char>st;
-        st.push(s[0]);
-        for(int i = 1; i < s.size(); i++){
-            if(!st.empty()){
-                if(s[i] == st.top()){
-                    st.pop();
-                    continue;
-                }
-            }
-            st.push(s[i]);
+        string temp = "";
+        for(int i = 0 ; i < s.size(); i++){
+            if(!temp.empty() && temp.back() == s[i]) temp.pop_back();
+            else temp += s[i];
         }
-        string ans;
-        while(!st.empty()){
-            ans += st.top();
-            st.pop();
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        return temp;
     }
 };
